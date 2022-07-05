d686bfce5807ef93bf506111bcc5c8f2956e3bf3
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-07-05 21:32:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Developed by Bharat Sharma
+
 import tkinter
 from pytube import YouTube
  
@@ -6,33 +8,23 @@ root.geometry('500x300')
 root.resizable(0,0)
 root.title("Developed by Bharat Sharma \n\n Youtube Video Downloader")
  
- 
 tkinter.Label(root, text ='Youtube Video Downloader', font ='arial 20 bold').pack()
- 
- 
- 
- 
+
 ##enter link
 link = tkinter.StringVar()
  
 tkinter.Label(root, text ='Paste Link Here:', font ='arial 15 bold').place(x= 160, y = 60)
 link_enter = tkinter.Entry(root, width = 70, textvariable = link).place(x = 32, y = 90)
  
- 
- 
 #function to download video
- 
- 
+
 def Downloader():
     
     url =YouTube(str(link.get()))
     video = url.streams.first()
     video.download()
     tkinter.Label(root, text ='DOWNLOADED', font ='arial 15').place(x= 180, y = 210)
- 
- 
+
 tkinter.Button(root, text ='DOWNLOAD', font ='arial 15 bold', bg ='blue', padx = 2, command = Downloader).place(x=180, y = 150)
- 
- 
- 
+
 root.mainloop()
